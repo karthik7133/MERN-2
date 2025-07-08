f9cfec84e1ebdb8833542945585691928e6120be
karthik7133 <chkarthik853@gmail.com>
2025-07-08 09:35:36 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,7 +29,7 @@ const Home = () => {
 
   const fetchCourses = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/api/courses');
+      const response = await axios.get('https://mern-2-backend.onrender.com/api/courses');
       setCourses(response.data);
     } catch (error) {
       console.error('Error fetching courses:', error);
